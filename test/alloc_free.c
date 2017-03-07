7d19cfd499912de3e6e5448a2571c6502a7f9cf3
Daniel Jour <musteresel@gmail.com>
2017-03-07 13:22:43 +0100

    Setting up build & test environment

---
+++ test/alloc_free.c
@@ -0,0 +1,16 @@
+#include <its.h>
+#include <stdio.h>
+
+int main() {
+  unsigned size;
+  unsigned failures = 0;
+  for (size = 0; size < 100; ++size) {
+    its const string = its_alloc(size);
+    if (! its_good(string)) {
+      ++failures;
+      fprintf(stderr, "Failed to allocate size %u\n", size);
+    }
+    its_free(string);
+  }
+  return (failures > 0);
+}
